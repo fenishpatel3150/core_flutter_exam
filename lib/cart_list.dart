373f6ebe16eba525160cb80d16a3c34281345bc8
Fenish Patel <patelfenish34@gmail.com>
2024-02-19 22:25:20 +0530

    19-02-2024

--- lib/cart_list.dart
+++ lib/cart_list.dart
@@ -20,11 +20,7 @@ class _CartListState extends State<CartList> {
             SizedBox(width: 450,),
             Text('New arrivals'),
             SizedBox(width: 20,),
-            GestureDetector(
-                onTap: (){
-                  Navigator.pushReplacementNamed(context,'/' );
-                },
-                child: Text('men')),
+            Text('men'),
             SizedBox(width: 20,),
             Text('women'),
             SizedBox(width: 20,),
@@ -38,6 +34,68 @@ class _CartListState extends State<CartList> {
 
           ],
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 50,left: 60),
+            child: Row(
+              children: [
+                Text('Your shopping cart',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              ],
+            ),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 800,
+                    color: Color(0xfff5f5f5),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15,left: 30),
+                    child: Row(
+                      children: [
+                        Container(height: 70,width: 70,color: Colors.white,
+                        child: Image.asset('asset/image/image3.png',fit: BoxFit.cover,),),
+                        SizedBox(width: 20,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+
+                            Text('Jordan Delta 2',style: TextStyle(fontWeight: FontWeight.bold),),
+                            Text('78,000RWF'),
+                          ],
+                        ),
+                        SizedBox(width: 150,),
+                        Icon(Icons.remove),
+                        SizedBox(width: 10,),
+                        Container(height: 30,width: 30,decoration: BoxDecoration(border: Border.all(),borderRadius: BorderRadius.circular(10)),),
+                        SizedBox(width: 10,),
+                        Icon(Icons.add),
+                        SizedBox(width: 200,),
+                        Text('78,000RWF'),
+
+
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              Column(
+                children: [
+                  Container(height: 500,width: 400,color:Colors.grey),
+                  Text('Order summary',),
+                ],
+              ),
+
+            ],
+          ),
+        ],
       ),
     );
   }
